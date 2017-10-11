c3342493f672c0e940178f04ef71b17ccd9197e8
Darknov <darknov.dono@gmail.com>
2017-10-11 15:26:49 +0200

    Kod z zajec: Dodanie odpowiedniej struktury katalogow w Assets, Block

---
+++ Assets/Scripts/PlatformCreator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformCreator : MonoBehaviour {
+
+    public GameObject block;
+
+	// Use this for initialization
+	void Start () {
+        float widthOfABlock = 1;
+
+        Instantiate(block, this.transform);
+
+        for (int i=0; i<2; i++)
+        {
+            GameObject newBlock = block;
+        }
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
