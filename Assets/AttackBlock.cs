cd2971e48f8bdc0d767c1e9c9971b461bb3b1168
Gnlknmd <dominik.lange@outlook.com>
2017-10-13 19:26:15 +0200

    Player 2 controller

---
+++ Assets/AttackBlock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBlock : MonoBehaviour {
+
+    public float jumpLength = 1;
+    public bool isBlockInMovement = false;
+    public Vector3 moveDirection;
+
+    private float moveSpeed;
+    private float jumpTime = 1;
+    private float currentTime;
+
+    public float MoveSpeed
+    {
+        set
+        {
+            this.moveSpeed = value;
+        }
+    }
+
+    void Start () {
+        if (moveDirection == null || moveDirection == Vector3.zero) throw new NullReferenceException("You must assign direction to " + this.name);
+
+        if (moveSpeed >= jumpTime) throw new ArgumentException("moveSpeed is too big. It should be < " + jumpTime);
+
+        if (moveSpeed == 0) throw new ArgumentException("moveSpeed of " + this.name + "cannot be 0");
+
+        jumpTime = 1 / moveSpeed;
+        currentTime = jumpTime;
+	}
+	
+	void Update () {
+
+        if (!isBlockInMovement) return;
+        
+        if (currentTime >= jumpTime)
+        {
+            gameObject.transform.position += moveDirection;
+            currentTime = 0;
+        }
+
+        currentTime += Time.deltaTime;// * moveSpeed;
+    }
+
+
+    public void Activate()
+    {
+        isBlockInMovement = true;
+    }
+}
---
+++ Assets/AttackBlocksController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackBlocksController : MonoBehaviour {
+
+	public AttackBlock northBlock, southBlock, eastBlock, westBlock;
+
+    public float blocksSpeed;
+
+	void Start () {
+		if(northBlock == null || southBlock == null || eastBlock == null || westBlock == null)
+        {
+            throw new System.NullReferenceException("You must assign all blocks to AttackBlocksController.");
+        }
+
+        // Setting movespeed doesn't work properly
+
+        northBlock.MoveSpeed = blocksSpeed;
+        southBlock.MoveSpeed = blocksSpeed;
+        eastBlock.MoveSpeed = blocksSpeed;
+        westBlock.MoveSpeed = blocksSpeed;
+	}
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W)) northBlock.Activate();
+		if (Input.GetKeyDown(KeyCode.S)) southBlock.Activate();
+		if (Input.GetKeyDown(KeyCode.A)) westBlock.Activate();
+        if (Input.GetKeyDown(KeyCode.D)) eastBlock.Activate();
+	}
+
+}
