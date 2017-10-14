a6eb4c92f64f7c6d374475b0e7fc1519f5ecdcb5
203956 <32635439+203956@users.noreply.github.com>
2017-10-14 18:59:53 +0200

    Prefabs, Normal Jump and TicTac
    Normal Jump w PlayerControler, Terrain to defaultowa plansza do chodzenia

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+	
+	public CharacterController characterControler;
+
+	public float predkoscPoruszania = 3.0f;
+	public float wysokoscSkoku = 6.0f;
+	private float aktualnaWysokoscSkoku = 0f;
+
+	void Start () {
+		characterControler = GetComponent<CharacterController>();
+	}
+
+	void Update() {
+		klawiatura();
+	}
+
+	private void klawiatura(){
+		float rochPrzodTyl = Input.GetAxis("Vertical") * predkoscPoruszania;
+		float rochLewoPrawo = Input.GetAxis("Horizontal") * predkoscPoruszania;
+
+		if(characterControler.isGrounded && Input.GetButton("Jump")){
+			aktualnaWysokoscSkoku = wysokoscSkoku;
+		} else if (!characterControler.isGrounded ){
+			aktualnaWysokoscSkoku += Physics.gravity.y * Time.deltaTime;
+		}
+
+		Debug.Log (Physics.gravity.y);
+
+		Vector3 ruch = new Vector3(rochLewoPrawo, aktualnaWysokoscSkoku, rochPrzodTyl);
+
+		ruch = transform.rotation * ruch;
+
+		characterControler.Move(ruch * Time.deltaTime);
+	}
+
+}
