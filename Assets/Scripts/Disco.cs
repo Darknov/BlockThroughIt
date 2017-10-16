2ec0c915157bc8255749717d8b2e28da51b44c41
Gnlknmd <dominik.lange@outlook.com>
2017-10-16 12:58:12 +0200

    Disco

---
+++ Assets/Scripts/Disco.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disco : MonoBehaviour {
+
+	Light light;
+
+	int i=0;
+	// Use this for initialization
+	void Start () {
+		light = gameObject.GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (i % 10 == 0) {
+
+			var random = new System.Random ();
+
+
+			Color color = new Color ();
+			color.r = (float)random.NextDouble () * 255;
+			color.b = (float)random.NextDouble () * 255;
+			color.g = (float)random.NextDouble () * 255;
+
+			light.color = color;
+		}
+
+		i++;
+
+
+	}
+}
