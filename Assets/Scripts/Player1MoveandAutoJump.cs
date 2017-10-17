1c75c1b018ace30648d1f28db66d547918dbc091
203956 <32635439+203956@users.noreply.github.com>
2017-10-17 20:30:39 +0200

    First

---
+++ Assets/Scripts/Player1MoveandAutoJump.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1MoveandAutoJump : MonoBehaviour
+{
+    public float velocity;
+    public float jumpHeight = 6.0f;
+    private float actualHeight = 0f;
+    private bool outOfTrigger = false;
+
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        float horizontalAxisJoystick = Input.GetAxis("HorizontalJoy");
+        float verticalAxisJoystick = Input.GetAxis("VerticalJoy");
+        RaycastHit hit;
+
+        Rigidbody rigidbody = transform.GetComponent<Rigidbody>();
+        Vector3 movement = Vector3.zero;
+
+        if ((int)horizontalAxisJoystick != 0 || (int)verticalAxisJoystick != 0)
+        {
+            movement = new Vector3(this.velocity * horizontalAxisJoystick, 0f, this.velocity * verticalAxisJoystick);
+        }
+
+        if (outOfTrigger == true)
+        {
+            rigidbody.AddForce(transform.up * 3.0f, ForceMode.Impulse);
+        }
+
+        if (this.velocity == 0.0f)
+        {
+            this.velocity = 0.025f;
+        }
+
+        movement = new Vector3(horizontalAxis * velocity, 0f, verticalAxis * velocity);
+
+        if (!movement.Equals(Vector3.zero))
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(new Vector3(movement.x, 0f, movement.z)), 0.15F);
+        }
+
+        //rigidbody.MovePosition(Vector3.Lerp(rigidbody.position, rigidbody.position + movement, 0.5f));
+        transform.Translate(movement * Time.deltaTime * 60, Space.World);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "platform")
+        {
+            outOfTrigger = true;
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "platform")
+    //    {
+    //        actualHeight = 0f;
+    //    }
+    //}
+}
