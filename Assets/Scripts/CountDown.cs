5c691ecd83f3e2f6b9dbc240e4d0b6471bab9273
203956 <32635439+203956@users.noreply.github.com>
2017-10-21 23:25:31 +0200

    countDown

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountDown : MonoBehaviour {
+
+	float timeRemaining = 60;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeRemaining -= Time.deltaTime;
+	}
+
+	void OnGUI()
+	{
+		if (timeRemaining > 0)
+		{
+			GUI.Label(new Rect(100, 100, 200, 100), "Time Remaining : " + (int)timeRemaining);
+		}
+		else
+		{
+			GUI.Label(new Rect(100, 100, 200, 100), "Time's Up");
+		}
+	}
+}
