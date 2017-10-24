2e6b502f9f1521409e6748ae68f807a4ff787432
203956 <32635439+203956@users.noreply.github.com>
2017-10-24 22:29:19 +0200

    Repaired Time Script

--- Assets/Scripts/CountDown.cs
+++ Assets/Scripts/CountDown.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class CountDown : MonoBehaviour {
 
+	public static float timeRemaining = 60;
 	Text text;
 
 	void Start () {
@@ -12,6 +13,10 @@ public class CountDown : MonoBehaviour {
 	}
 
 	void Update () {
-		text.text = "Time Ramaining: " + (int)GameOver.timeRemaining;
+		text.text = "Time Ramaining: " + (int)timeRemaining;
+
+		if (timeRemaining >= 0 && GameObject.FindWithTag("Player")) {
+			timeRemaining -= Time.deltaTime;
+		}
 	}
 }
--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,21 +4,14 @@ using UnityEngine;
 
 public class GameOver : MonoBehaviour {
 
-	public static float timeRemaining = 60;
 	Animator anim;
 
-	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		if (timeRemaining >= 0 && GameObject.FindWithTag("Player")) {
-			timeRemaining -= Time.deltaTime;
-		}
-			
-		if (timeRemaining <= 0) {
+		if (CountDown.timeRemaining <= 0) {
 			anim.SetTrigger ("GameOver1");
 		}
 
