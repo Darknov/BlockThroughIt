4a23068a369cfa7e567d3fde1c60e5627498a9f1
Darknov <darknov.dono@gmail.com>
2017-11-20 19:15:33 +0100

    Added some effects for background

---
+++ Assets/Scripts/backgroundObjectEffects/TransformBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformBehaviour : MonoBehaviour {
+
+    // Use this for initialization
+    public bool isExpanding = true;
+    public bool isChangingHeight = true;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(isExpanding)
+            this.transform.localScale += new Vector3( Mathf.Sin(Time.timeSinceLevelLoad) / 120.0f, Mathf.Cos(Time.timeSinceLevelLoad) / 120.0f, 0);
+        if(isChangingHeight)
+            this.transform.position += new Vector3(0, Mathf.Sin(Time.timeSinceLevelLoad)/ 300.0f, 0);
+	}
+}
