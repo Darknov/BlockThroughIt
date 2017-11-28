02f6f4040982798ab9d4c30c5ca5c57fdec3e840
Darknov <darknov.dono@gmail.com>
2017-11-28 22:23:43 +0100

    Generate items on random positions on platform Board

---
+++ Assets/Scripts/SpawnItem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItem : MonoBehaviour {
+    public GameObject[] items;
+    public float frequency = 1;
+    public int maxItems = 3;
+    public PlatformBoard platformBoard;
+    private GameObject[,] generatedItems;
+	// Use this for initialization
+	void Start () {
+        generatedItems = new GameObject[platformBoard.rowLength, platformBoard.rowLength];
+        InvokeRepeating("spawnRandomItem", 1, frequency);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void spawnRandomItem()
+    {
+        int howMany = 0;
+        for(int i = 0; i < platformBoard.rowLength; i++)
+        {
+            for(int j = 0; j < platformBoard.rowLength; j++)
+            {
+                if(generatedItems[i,j] != null)
+                {
+                    howMany++;
+                }
+            }
+        }
+        int howManyBlocks = 0;
+        for (int i = 0; i < platformBoard.rowLength; i++)
+        {
+            for (int j = 0; j < platformBoard.rowLength; j++)
+            {
+                if (platformBoard.blocks[i, j] != null)
+                {
+                    howManyBlocks++;
+                }
+            }
+        }
+        if (howMany < maxItems && howMany < howManyBlocks)
+        {
+            int number = Random.Range(0, items.Length);
+            int x;
+            int y;
+            do
+            {
+                x = Random.Range(0, platformBoard.rowLength);
+                y = Random.Range(0, platformBoard.rowLength);
+                Debug.Log(x + " " + y);
+            } while (platformBoard.blocks[x, y] == null || generatedItems[x, y] != null);
+            Vector3 vector = new Vector3(platformBoard.transform.position.x + x + platformBoard.widthOfAGap * x, platformBoard.transform.position.y + 1, platformBoard.transform.position.z + y + platformBoard.widthOfAGap * y);
+
+            generatedItems[x, y] = Instantiate(items[number], vector, Quaternion.identity);
+            Debug.Log(vector);
+        }
+    }
+}
