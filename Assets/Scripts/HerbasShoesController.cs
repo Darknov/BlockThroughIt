532cafbc03369dfb2dc5eaa171c6ae2dcc9102e3
Ada <adunia303@o2.pl>
2017-12-13 00:58:21 +0100

    herbas particle effect

--- Assets/Scripts/HerbasShoesController.cs
+++ Assets/Scripts/HerbasShoesController.cs
@@ -5,7 +5,7 @@ public class HerbasShoesController : BoostItem {
 
     private bool isActive = false;
     public float initialFlyingDuration = 5f;
-
+    public GameObject partEffect;
     private float flyingDuration;
 
     private void Start()
@@ -16,6 +16,7 @@ public class HerbasShoesController : BoostItem {
     public override void ActivateItem()
     {
         this.isActive = true;
+        Instantiate(partEffect, new Vector3( player.transform.position.x, player.transform.position.y, player.transform.position.z) , player.transform.rotation, player.transform);
     }
 
 	void Update() {
@@ -32,7 +33,22 @@ public class HerbasShoesController : BoostItem {
 				player.GetComponent<Rigidbody>().useGravity = true;
                 isActive = false;
                 flyingDuration = initialFlyingDuration;
-		}
-	}
+        /*    foreach (var item in partEffect.GetComponentsInChildren<Transform>())
+            {
+                Destroy(item.gameObject);
+                Destroy(item.particleSystem.gameObject);
+            }
+            foreach (var item in partEffect.GetComponentsInParent<Transform>())
+            {
+                Destroy(item.gameObject);
+            }*/
+           // Destroy(partEffect.particleSystem.gameObject);
+           // partEffect.particleEmitter.emit = false;
+            partEffect.transform.parent = null; // detach particle system
+            Destroy(partEffect.gameObject, 3);
+            Destroy(gameObject);
+
+        }
+    }
 
 }
