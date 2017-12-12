2746c4d9eb150831b214fcbf50609949d9205774
Aleksandra <dziobiaka@gmail.com>
2017-12-12 18:16:36 +0100

    added background music + changing volume

---
+++ Assets/Scripts/VolumeControll.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeControll : MonoBehaviour {
+
+    public static float vol = 0.5f;
+    public Slider volume;
+
+    void Start () {
+       volume.value = vol;
+    }
+
+    private void Update()
+    {
+        AudioManager.instance.ChangeVolume(volume.value);
+    }
+}
