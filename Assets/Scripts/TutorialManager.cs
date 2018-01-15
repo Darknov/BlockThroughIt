5f661e4e1c5ff8cc799236e672fa20de927c91b6
Darknov <darknov.dono@gmail.com>
2018-01-15 17:11:11 +0100

    changing screens in tutorial

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour {
+
+    public Texture[] textures;
+
+
+    private int textureNumber;
+    private int previousNumber;
+    // Use this for initialization
+    void Start () {
+        textureNumber = 0;
+        previousNumber = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+        {
+            if (textureNumber < textures.Length)
+            {
+                textureNumber++;
+            }
+            else
+            {
+                // tutaj dodac zmiane sceny na menuScene
+            }
+            
+
+        }
+        if(textureNumber != previousNumber)
+        {
+            previousNumber = textureNumber;
+            GetComponent<Renderer>().material.mainTexture = textures[textureNumber];
+        }
+	}
+}
