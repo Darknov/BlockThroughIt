074e0ce4092e1129db11c31e72976ca342bb0f6e
Ada <adunia303@o2.pl>
2018-01-16 23:36:56 +0100

    God can win now

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -4,41 +4,49 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
-public class GameOver : MonoBehaviour {
+public class GameOver : MonoBehaviour
+{
 
-	public float restartDelay = 2f;
+    public float restartDelay = 2f;
 
-	Animator anim;
-	float restartTimer;
+    Animator anim;
+    float restartTimer;
 
-	void Start () {
-		anim = GetComponent<Animator>();
+    void Start()
+    {
+        anim = GetComponent<Animator>();
     }
 
-    void Update () {
-        if (CountDown.timeRemaining >= 120 || CountDown.timeRemaining <= 0)
+    void Update()
+    {
+        if (CountDown.timeRemaining >= 120)
         {
-            anim.SetTrigger ("GameOver1");
-			CountDown.started = false;
-			restartTimer += Time.deltaTime;
-			if (restartTimer >= restartDelay) {
-                SceneManager.LoadScene (0);
-				if (CountDown.timeRemaining != 60) {
+            anim.SetTrigger("GameOver1");
+            CountDown.started = false;
+            restartTimer += Time.deltaTime;
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(0);
+                if (CountDown.timeRemaining != 60)
+                {
                     CountDown.timeRemaining = 60;
-				}
-			}
-		}
+                }
+            }
+        }
 
-		if (!GameObject.FindWithTag("Player")) {
-			anim.SetTrigger ("GameOver2");
-			CountDown.started = false;
-			restartTimer += Time.deltaTime;
-			if (restartTimer >= restartDelay) {
-				SceneManager.LoadScene (0);
-				if (CountDown.timeRemaining != 60) {
-					CountDown.timeRemaining  = 60;
-				}
-			}
-		}
-	}
+        if (!GameObject.FindWithTag("Player") || CountDown.timeRemaining <= 0)
+        {
+            anim.SetTrigger("GameOver2");
+            CountDown.started = false;
+            restartTimer += Time.deltaTime;
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(0);
+                if (CountDown.timeRemaining != 60)
+                {
+                    CountDown.timeRemaining = 60;
+                }
+            }
+        }
+    }
 }
