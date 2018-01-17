4ee9d14f5258488bf1a563b6de275cc7b255f8b8
Darknov <darknov.dono@gmail.com>
2018-01-17 06:23:34 +0100

    merge

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialManager : MonoBehaviour {
+
+    public Texture[] textures;
+
+
+    private int textureNumber;
+    private int previousNumber;
+    // Use this for initialization
+    void Start () {
+        textureNumber = 0;
+        previousNumber = -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.anyKeyDown)
+        {
+            if (textureNumber < textures.Length- 1)
+            {
+                textureNumber++;
+            }
+            else
+            {
+                // tutaj dodac zmiane sceny na menuScene
+                SceneManager.LoadScene(0, LoadSceneMode.Single);
+            }
+            
+
+        }
+        if(textureNumber != previousNumber)
+        {
+            previousNumber = textureNumber;
+            GetComponent<Renderer>().material.mainTexture = textures[textureNumber];
+        }
+	}
+}
